268d256bc0c2f55cb29712999322199fca70ab5c
William Song <30965609+Freakwill@users.noreply.github.com>
2018-11-26 20:43:19 +0800

    Add files via upload

--- name_gender.py
+++ name_gender.py
@@ -6,10 +6,11 @@ import numpy as np
 
 import nltk
 
-path = 'science.xls' # name list of students in zjc
+path = 'namelist.xls' # name list of students in zjc
 
-for n in range(13, 18):
-    class_ = 'xinji%d' % n
+classes = ['xinji%d' % n for n in range(13, 18)] + ['xinjiang1', 'xinjiang2']
+for n, class_ in enumerate(classes):
+    
     df = pd.read_excel(path, sheet_name=class_)
     if not (set(df.columns) & {'学号','no','No', 'no.', 'No.'}):
         df = pd.read_excel(path, sheet_name=class_, skiprows=1)
@@ -23,7 +24,7 @@ for n in range(13, 18):
         gender_key = '性别'
     df = df[[name_key, gender_key]]
     df.dropna()
-    if n == 13:
+    if n == 0:
         data = df.values
     else:
         data = np.vstack((data, df.values))
@@ -45,28 +46,35 @@ def get_features(df, get_feature):
         d = dict(row[1])
         name = d['name']
         if isinstance(name, str):
+            if ' ' in name:
+                name = name.replace(' ', '')
             if '(' not in name:
-                featrues.append((get_feature(name), d['gender'].strip('()')))
+                featrues.append((get_feature(name), d['gender'].strip('() ')))
             else:
                 name = name.partition('(')[0]
-                featrues.append((get_feature(name), d['gender'].strip('()')))
+                featrues.append((get_feature(name), d['gender'].strip('() ')))
     return featrues
 
-def example1():
-    featrues = get_features(df, get_feature)
-
-    # train-test
+def get_train_test(featrues):
+    # split the data into train part and test part
     N = len(featrues)
     T = int(N * 0.2)
     train = featrues[:T]
     test = featrues[T:]
+    return train, test
+
+def example1():
+    featrues = get_features(df, get_feature)
+    train, test = get_train_test(featrues)
     classifier = nltk.NaiveBayesClassifier.train(featrues)
     acc = nltk.classify.accuracy(classifier, test)
     print(acc)
 
     # predict
-    gender = classifier.classify(get_feature('李春娜'))
-    print(gender)
+    new_name = '李春娜'
+    gender = classifier.classify(get_feature(new_name))
+    print('李春娜', gender)
+    classifier.show_most_informative_features(10)
 
 import pypinyin
 def get_feature_pinyin(name):
@@ -77,19 +85,16 @@ def get_feature_pinyin(name):
 
 def example2():
     featrues = get_features(df, get_feature_pinyin)
-
-    # train-test
-    N = len(featrues)
-    T = int(N * 0.2)
-    train = featrues[:T]
-    test = featrues[T:]
+    train, test = get_train_test(featrues)
     classifier = nltk.NaiveBayesClassifier.train(featrues)
     acc = nltk.classify.accuracy(classifier, test)
     print(acc)
 
     # predict
-    gender = classifier.classify(get_feature('李春娜'))
-    print(gender)
+    new_name = '李春娜'
+    gender = classifier.classify(get_feature(new_name))
+    print('李春娜', gender)
+    classifier.show_most_informative_features(5)
 
 print('With Chinese:')
 example1()
