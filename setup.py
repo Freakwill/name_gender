1ab2fde2a657fc648cde93e6a211fcdff57cfb48
William Song <30965609+Freakwill@users.noreply.github.com>
2020-10-24 10:36:22 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,29 +2,38 @@
 
 from setuptools import setup
 
+DESCRIPTION = "Name => Gender"
+
+with open("README.md") as f:
+    LONG_DESCRIPTION = f.read()
+ 
+KEYWORDS = "Naive Bayes"
+
 setup(
-    name='name_gender',
-    version='0.1',
-    url='https://github.com/Freakwill/name_gender',
-    license='MIT',
-    author='William Song',
-    description='Name => Gender',
-    long_description=__doc__,
+    name = 'name_gender',
+    py_modules = ['name_gender'],
+    version = "0.1",
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    
     install_requires=[
         'numpy',
         'pandas'
     ],
-    tests_require = ['nose', 'pytest'],
-    py_modules=['name_gender'],
-    zip_safe=False,
-    include_package_data=True,
-    platforms='any',
-    classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+    classifiers = [
+        'License :: Public Domain',  # Public Domain
         'Programming Language :: Python :: 3',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
         'TOPIC :: SCIENTIFIC/ENGINEERING :: ARTIFICIAL INTELLIGENCE',
         'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+    ],
+    keywords = KEYWORDS,
+    author = "William Song",
+    author_email = "songcwzjut@163.com",
+    url = "https://github.com/Freakwill/name_gender",
+    license = "MIT",
+    # packages = PACKAGES,
+    include_package_data=True,
+    zip_safe=True,
 )
