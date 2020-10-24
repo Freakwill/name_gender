03341e3f9baacf4b6c1a722b895f1c73851f9a2b
William Song <30965609+Freakwill@users.noreply.github.com>
2020-10-24 10:45:55 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,20 @@
 from name_gender import *
 
-print('With Chinese:')
-show_gender("蔡徐坤")
-print('With Pinyin:')
-show_gender("蔡徐坤", True)
-print('取名字:(给出性别和第一个字)')
-give_name()
+import pytest
+class Test_NG:
+    # 函数级开始
+    def test_chinese(self):
+        print('With Chinese:')
+        show_gender("蔡徐坤")
+        return True
+    def test_pinyin(self):
+        print('With Pinyin:')
+        show_gender("蔡徐坤", True)
+        return True
+    def test_give_name(self):
+        print('取名字:(给出性别和第一个字)')
+        give_name()
+        return True
+        
+if __name__ == '__main__':
+    pytest.main("-s test_ng.py")
